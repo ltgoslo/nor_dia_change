f9b374083f490898faf33cd5a5adfdc8616b3089
garrafao <schlecdk@wiesenweihe.ims.uni-stuttgart.de>
2025-02-18 21:53:02 +0100

    Version: 1.0.1

---
+++ subset1/plots/opt/weight/stats_plotting.js
@@ -0,0 +1,318 @@
+stats_plotting = [
+    {
+        "lemma": "anfektelse",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "anfektelse",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "bit",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "bit",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "b\u00f8lge",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "b\u00f8lge",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "damp",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "damp",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "data",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "data",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "egg",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "egg",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "etterforskning",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "etterforskning",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "fag",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "fag",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "fil",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "fil",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "forhold",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "forhold",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "formiddagen",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "formiddagen",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "frakk",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "frakk",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "gress",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "gress",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "horisont",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "horisont",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "husrom",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "husrom",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "idiot",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "idiot",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "kart",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "kart",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "katt",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "katt",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "kjemi",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "kjemi",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "kloakk",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "kloakk",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "landet",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "landet",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "leder",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "leder",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "leilighet",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "leilighet",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "linse",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "linse",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "lyng",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "lyng",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "m\u00e5l",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "m\u00e5l",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "mening",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "mening",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "operasjon",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "operasjon",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "\u00f8rret",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "\u00f8rret",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "p\u00e6re",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "p\u00e6re",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "plattform",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "plattform",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "rad",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "rad",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "rev",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "rev",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "ris",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "ris",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "sete",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "sete",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "skjerm",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "skjerm",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "stoff",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "stoff",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "tro",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "tro",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "tropp",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "tropp",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "varsel",
+        "node_position": "spring"
+    }
+]
---
+++ subset2/plots/opt/weight/stats_plotting.js
@@ -0,0 +1,318 @@
+stats_plotting = [
+    {
+        "lemma": "bank",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "bank",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "bystyrerepresentant",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "bystyrerepresentant",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "driftsregnskap",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "driftsregnskap",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "fane",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "fane",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "fj\u00e6r",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "fj\u00e6r",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "innstilling",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "innstilling",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "kanal",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "kanal",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "kj\u00f8pepress",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "kj\u00f8pepress",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "kode",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "kode",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "kohort",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "kohort",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "kreft",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "kreft",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "mappe",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "mappe",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "melding",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "melding",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "melk",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "melk",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "moment",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "moment",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "mus",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "mus",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "nett",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "nett",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "oppvarming",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "oppvarming",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "overflate",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "overflate",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "papir",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "papir",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "r\u00e5dgiver",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "r\u00e5dgiver",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "s\u00e6rforbund",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "s\u00e6rforbund",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "side",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "side",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "sky",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "sky",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "spill",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "spill",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "storby",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "storby",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "str\u00f8m",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "str\u00f8m",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "stryk",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "stryk",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "Syden",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "Syden",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "tape",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "tape",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "tavle",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "tavle",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "tendens",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "tendens",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "terror",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "terror",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "test",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "test",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "tjener",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "tjener",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "utakt",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "utakt",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "vert",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "vert",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "vindu",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "vindu",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "virkelighet",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "virkelighet",
+        "node_position": "spring"
+    },
+    {
+        "lemma": "virus",
+        "node_position": "spring"
+    }
+]
